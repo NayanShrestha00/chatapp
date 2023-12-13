6a5fc6097e7775c44ab621f3e0540ce87c405b57
bishalrajbanshi <user.bishalrajbanshi@gmail.com>
2023-12-13 08:45:24 +0545

    second commit

--- src/chatapp/Chatapp.java
+++ src/chatapp/Chatapp.java
@@ -20,25 +20,28 @@ public class Chatapp {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        String url = "jdbc:sqlserver://bitproject.database.windows.net;databaseName=chatapp";
-    String user = "bala";
-    String password = "nobita@00";
-    try{
-        Connection connection = DriverManager.getConnection(url, user, password);
-        System.out.println("Connected Successfully");
-//        String sql = "CREATE TABLE demo(name VARCHAR(50))";
-//        String sql= "insert into new " + "Values('kala')"; 
-        String sql = "insert into Ouruser(user_name, user_email, user_mobile)" + "values('kala','C','D')";
-        Statement statement = connection.createStatement();
-        int row = statement.executeUpdate(sql);
-        if (row > 0){
-            System.out.println("Import sucess");
-        }
-         
-        connection.close();
-    } catch(SQLException e){
-        System.out.println("Opps!");
+//        String url = "jdbc:sqlserver://bitproject.database.windows.net;databaseName=chatapp";
+//        String user = "bala";
+//        String password = "nobita@00";
+//        try {
+//            Connection connection = DriverManager.getConnection(url, user, password);
+//            System.out.println("Connected Successfully");
+////        String sql = "CREATE TABLE demo(name VARCHAR(50))";
+////        String sql= "insert into demo" + "Values('kala')"; 
+//            String sql = "insert into demo (name)" + "values('kal')";
+//
+//            Statement statement = connection.createStatement();
+//            int row = statement.executeUpdate(sql);
+//            if (row > 0) {
+//                System.out.println("Import sucess");
+//            }
+//
+//            connection.close();
+//        } catch (SQLException e) {
+//            System.out.println("Opps!");
+//            System.out.println(e);
+//
+//        }
     }
-    }
-    
+
 }
